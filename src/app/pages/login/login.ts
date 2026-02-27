a2f3e172141ae00e1ffe083f5e0e5cc6ac89de4f
Natalia Zárate <nataliazarate213@gmail.com>
2026-02-27 12:59:29 -0600

    Diseño segunda página

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [Navbar],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  constructor(private router: Router) { }
+
+  ingresar() {
+    this.router.navigate(['/gestion-incidencias']);
+  }
 
 }
