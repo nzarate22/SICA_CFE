6c33fa4e57a780315d6ffad939f12e6e84f307ed
Natalia Zárate <nataliazarate213@gmail.com>
2026-03-03 11:26:19 -0600

    Diseño mejorado y ceación de modelo

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
 import { Route, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [Navbar],
+  imports: [CommonModule, FormsModule, Navbar],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  mostrarPassword: boolean = false;
+
   constructor(private router: Router) { }
+
+  togglePassword() {
+    this.mostrarPassword = !this.mostrarPassword;
+  }
 
   ingresar() {
     this.router.navigate(['/gestion-incidencias']);
