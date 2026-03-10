5bdb1d92c1d914e1f75467958b184790797557bd
Natalia Zárate <nataliazarate213@gmail.com>
2026-03-10 12:49:46 -0600

    Correción de conteos de retardos y salidas anticipadas

--- src/app/services/eventos.ts
+++ src/app/services/eventos.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
@@ -7,15 +7,18 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class Eventos {
-
   private readonly URL_API = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   obtenerAsistencias(rpe: string, fechaInicio: string, fechaFin: string): Observable<any> {
 
-    const url = `${this.URL_API}/get_eventos.php?rpe=${rpe}&fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`;
-    return this.http.get(url);
+    const params = new HttpParams()
+      .set('rpe', rpe)
+      .set('fechaInicio', fechaInicio)
+      .set('fechaFin', fechaFin);
+
+    return this.http.get(`${this.URL_API}/get_eventos.php`, { params });
   }
 }
 
